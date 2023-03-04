ac27379336d12f6ee2308da34538a5e6d74f7023
LeeTaeKyung <leek9708@gmail.com>
2023-03-04 10:27:45 +0900

    payload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { plusOne, minusOne } from "./redux/modules/counter";
+import { plusOne, minusOne, plusN, minusN } from "./redux/modules/counter";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
   //여기에서 스토어에 접근하여, counter 의 값을 읽어오고 싶다! (6*)
   // 그때 사용하는 리덕스의 훅이 useSelector
   // 매개변수로 콜백함수가 들어가요
@@ -21,20 +24,30 @@ function App() {
   return (
     <>
       <div>현재카운트 : {counter.number}</div>
+      <div>
+        <input
+          type="number"
+          value={number}
+          onChange={(e) => {
+            setNumber(+e.target.value); // +로 해주면 형변환
+          }}
+        />
+      </div>
       <button
         onClick={() => {
           //+1을 해주는 로직을 써주면 된다.
           //dispatch({type : PLUS_ONE,})
           // counter.js로 넘어가기 (7*)
-          dispatch(plusOne()); //인자로 액션 객체를 넣어줘야 한다 키,밸류 타입과 payload
+          // dispatch(plusOne()); //인자로 액션 객체를 넣어줘야 한다 키,밸류 타입과 payload
+          dispatch(plusN(number));
         }}
       >
         +
       </button>
       <button
         onClick={() => {
-          dispatch(minusOne());
-        }}
+          dispatch(minusN(number));
+        }} // 작동을 안했는데 그 이유는 리듀서 부분을 수정을 안해줘서임
       >
         -
       </button>
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -2,6 +2,8 @@
 // 이후 app.js가서 써주자 (12*)
 export const PLUS_ONE = "counter/PLUS_ONE";
 export const MINUS_ONE = "counter/MINUS_ONE";
+export const PLUS_N = "counter/PLUS_N";
+export const MINUS_N = "counter/MINUS_N";
 
 //action creator : action value를 액셕객체로 return 하는 함수
 export const plusOne = () => {
@@ -9,6 +11,19 @@ export const plusOne = () => {
 };
 export const minusOne = () => {
   return { type: MINUS_ONE };
+};
+export const plusN = (payload) => {
+  return {
+    type: PLUS_N,
+    payload: payload,
+  };
+};
+
+export const minusN = (payload) => {
+  return {
+    type: MINUS_N,
+    payload: payload,
+  };
 };
 
 // 초기 상태값 (state)
@@ -24,12 +39,18 @@ const initialState = {
 // state = initialState 로 적어줌
 // action이 인자로 들어올 때 action은 객체 형태로 되어있고 객체는 액션의 타입과 밸류를 가지고 있다.
 // 액션 = 스테이트를 어떻게 수정할건지 액션에 대해 표현하는 액션객체
+// action 객체라는 것은 action type을 payload 만큼 처리하는 것이다 (payload 1*)
+// ex : plyload가 3이다. 3만큼을 action type을 해라
 const counter = (state = initialState, action) => {
   switch (action.type) {
     case PLUS_ONE: //액션에 있는 타입을 플러스 원으로 던져주면 되겠다 다시 app.js로 (8*)
       return { number: state.number + 1 }; //객체로 넣어줘야한다. (10*)
     case MINUS_ONE: // 이렇게 바꾸면 리듀서랑 실제 컴포넌트 호출 부분을 맞춰줘야 한다. 그런데 만약 리듀서를 200개 컴포넌트가 쓴다면? 하나하나씩 전부 변경해줘야한다. 그런 하드코딩 방식은 지양해야함. 그래서 액션 크리에이터, 액션밸류가 나옴 변수 형태로 관리하면 어때? 위에서 액션 밸류 만들어주자(11*)
       return { number: state.number - 1 };
+    case PLUS_N:
+      return { number: state.number + action.payload };
+    case MINUS_N:
+      return { number: state.number - action.payload };
     default:
       return state; // 아무것도 없을 땐 최초 스테이트를 리턴해주면 된다는 소리
   }
