f2f86aa3f216b2f91b3218ff8b3f2f9a3b376e3a
LeeTaeKyung <leek9708@gmail.com>
2023-03-04 10:54:34 +0900

    리덕스 개념은 끝

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ root.render(
   </Provider>
 );
 
-// Provider는 해주고 import {Provider} import store 해주고
+// Provider는 해주고 import {Provider},  import store 해주고
+// App이라는 컴포넌트가 provider 지배권 안으로 들어온다.
+// store를 기반으로 지배권을 행사함.
 // 모듈스 -> 카운터.js 로 넘어가기 (2*)
 
 // If you want to start measuring performance in your app, pass a function
