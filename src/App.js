5dc7f4bc2ff7dc73ef3a1624a366108d0b292775
LeeTaeKyung <leek9708@gmail.com>
2023-03-04 09:53:30 +0900

    action value & creator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
+import { plusOne, minusOne } from "./redux/modules/counter";
 
 function App() {
   //여기에서 스토어에 접근하여, counter 의 값을 읽어오고 싶다! (6*)
@@ -23,19 +24,16 @@ function App() {
       <button
         onClick={() => {
           //+1을 해주는 로직을 써주면 된다.
+          //dispatch({type : PLUS_ONE,})
           // counter.js로 넘어가기 (7*)
-          dispatch({
-            type: "PLUS_ONE",
-          }); //인자로 액션 객체를 넣어줘야 한다 키,밸류 타입과 payload
+          dispatch(plusOne()); //인자로 액션 객체를 넣어줘야 한다 키,밸류 타입과 payload
         }}
       >
         +
       </button>
       <button
         onClick={() => {
-          dispatch({
-            type: "MINUS_ONE",
-          });
+          dispatch(minusOne());
         }}
       >
         -
